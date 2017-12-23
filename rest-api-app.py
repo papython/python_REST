a944c3fdf55ab749fb403ec769fb98a9693be049
Prakash Anand <prakashanand.email@gmail.com>
2017-12-22 22:05:50 -0500

    Made a lot of progress with Flask

--- rest-api-app.py
+++ rest-api-app.py
@@ -6,8 +6,21 @@
 import commands
 from flask import Flask 
 from flask import jsonify
+from flask import request
+from flask import render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+bootstrap = Bootstrap(app)
+
+class NameForm(FlaskForm):   
+ name = StringField('What is your name?', validators=[DataRequired()])
+ submit = SubmitField('Submit')
 
 @app.route('/ls')
 def ls():
@@ -23,7 +36,29 @@ def ps():
     print "number of /ps requests = " + str(ps_request)
     return jsonify(commands.getoutput("ps -ef"))
 
+@app.route('/h/<name>')
+def default(name=None):
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
+
+@app.route('/headers')
+def headers():
+    c = []
+    c.append(request.headers)
+    c.append(request.endpoint)
+    c.append(request.path)
+    c.append(request.method)
+    c.append(request.host)
+    return '<h1>{}</h1>'.format(c)
+
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html',name=name)
+
 if __name__ == '__main__':
     ls_request = 0
     ps_request = 0
-    app.run(host='0.0.0.0',port=81,debug=False)
+    app.run(host='0.0.0.0',port=8081,debug=False)
